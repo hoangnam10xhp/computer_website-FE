c8adf19ad9ed66e5f7aad240ce093e667929472c
hoangnam10xhp <hoangnam10xhp@gmail.com>
2024-09-06 11:54:50 +0700

    Initial commit

---
+++ resources/views/home/components/slider.blade.php
@@ -0,0 +1,34 @@
+@php
+    $baseUrl = 'http://127.0.0.1:8081/storage';
+@endphp
+
+<section id="slider"><!--slider-->
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <div id="slider-carousel" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        @foreach ($banners as $key => $banner)
+                            <li data-target="#slider-carousel" data-slide-to="{{ $key }}" class="{{ $key == 0 ? 'active' : '' }}"></li>
+                        @endforeach
+                    </ol>
+
+                    <div class="carousel-inner">
+                        @foreach ($banners as $key => $banner)
+                            <div class="item {{ $key == 0 ? 'active' : '' }}">
+                                <img src="{{ $baseUrl . '/' . $banner->image_path }}" alt="Banner" />
+                            </div>
+                        @endforeach
+                    </div>
+
+                    <a href="#slider-carousel" class="left control-carousel hidden-xs" data-slide="prev">
+                        <i class="fa fa-angle-left"></i>
+                    </a>
+                    <a href="#slider-carousel" class="right control-carousel hidden-xs" data-slide="next">
+                        <i class="fa fa-angle-right"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section><!--/slider-->
